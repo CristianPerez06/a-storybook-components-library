f4757a354873412cf97100b14261ca1e644b51bf
Cristian Perez <cristian.ap84@gmail.com>
2022-09-07 11:13:33 -0300

    Improved pre-commit. Minor story fixes.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,15 @@
+// eslint-disable-next-line no-undef
 module.exports = {
   env: {
     browser: true,
     es2021: true,
   },
-  extends: ['plugin:react/recommended', 'standard-with-typescript', 'prettier'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
   overrides: [],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['react'],
-  rules: {},
+  plugins: ['@typescript-eslint'],
 }
